62c1306eff8a84b18ab5f019990c237f5f3bc9dd
AllenHsing3 <allen.hsing@gmail.com>
2020-08-12 15:17:52 +0800

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path')
 
 const app = express();
 app.use(express.json({ extended: false }));
 
 app.use('/user', require('./routes/user'));
 app.use('/card', require('./routes/card'))
+
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder:
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 connectDB();
 
